9b17fb66c6fdb12c04c9b32432ec783f96e4de6c
Ivan Gospodinov <ivandiqnov1@gmail.com>
2017-10-26 10:55:13 +0300

    Created the Dispatcher that listens at /index and added logic to check if the user is logged in, checking the session, if yes send to index.jsp, if not send to login/register jsp

---
+++ TravelShare/src/main/java/com/travelshare/controller/Dispatcher.java
@@ -0,0 +1,21 @@
+package com.travelshare.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/index")
+public class Dispatcher {
+
+	@RequestMapping()
+	public String sayHello(Model model,  HttpSession session) {
+ 		if(session.getAttribute("user") == null) {
+			return "login";
+		}
+		return "index";
+	}
+	
+}
